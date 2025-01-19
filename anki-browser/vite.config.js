66f825fd1cdfaa4faa9b6a5661ab4d40b5ada315
Jim Slattery <slattery.jim@gmail.com>
2025-01-19 01:08:21 -0500

    following some setup from Claude
    https://claude.ai/chat/fc707bda-2d44-4556-828d-4a823b3121da
    
    Had to set the base dynamically for now, since I'm currently publishing /dist/ as a subfolder for Github Pages.

--- anki-browser/vite.config.js
+++ anki-browser/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/anki-browser/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/anki-browser/dist/' : '/anki-browser/',
   plugins: [react()],
-})
+}))
